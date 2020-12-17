4a1a665fedfe6441520af41f22bd378e89e2c93e
QuentinSauvage <sauvage.quentin62@gmail.com>
2020-12-17 14:07:21 +0100

    Fix DebugLogger

--- Assets/Scripts/Utils/DebugLogger.cs
+++ Assets/Scripts/Utils/DebugLogger.cs
@@ -8,139 +8,158 @@ namespace com.CompanyR.FrameworkR.Utils
 	{
 		public static void Log(object message, Object context)
 		{
-#if UNITY_EDITOR
-			Debug.Log(message, context);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.Log(message, context);
+			}
 		}
 
 		public static void Log(object message)
 		{
-#if UNITY_EDITOR
-			Debug.Log(message);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.Log(message);
+			}
 		}
 
 		[System.Diagnostics.Conditional("UNITY_ASSERTIONS")]
 		public static void LogAssertion(object message, Object context)
 		{
-#if UNITY_EDITOR
-			Debug.LogAssertion(message, context);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogAssertion(message, context);
+			}
 		}
 
 		[System.Diagnostics.Conditional("UNITY_ASSERTIONS")]
 		public static void LogAssertion(object message)
 		{
-#if UNITY_EDITOR
-			Debug.LogAssertion(message);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogAssertion(message);
+			}
 		}
 
 		[System.Diagnostics.Conditional("UNITY_ASSERTIONS")]
 		public static void LogAssertionFormat(Object context, string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogAssertionFormat(context, format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogAssertionFormat(context, format, args);
+			}
 		}
 
 		[System.Diagnostics.Conditional("UNITY_ASSERTIONS")]
 		public static void LogAssertionFormat(string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogAssertionFormat(format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogAssertionFormat(format, args);
+			}
 		}
 
 		public static void LogError(object message)
 		{
-#if UNITY_EDITOR
-			Debug.LogError(message);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogError(message);
+			}
 		}
 
 		public static void LogError(object message, Object context)
 		{
-#if UNITY_EDITOR
-			Debug.LogError(message, context);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogError(message, context);
+			}
 		}
 
 		public static void LogErrorFormat(string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogErrorFormat(format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogErrorFormat(format, args);
+			}
 		}
 
 		public static void LogErrorFormat(Object context, string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogErrorFormat(context, format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogErrorFormat(context, format, args);
+			}
 		}
 
 		public static void LogException(System.Exception exception)
 		{
-#if UNITY_EDITOR
-			Debug.LogException(exception);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogException(exception);
+			}
 		}
 
 		public static void LogException(System.Exception exception, Object context)
 		{
-#if UNITY_EDITOR
-			Debug.LogException(exception, context);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogException(exception, context);
+			}
 		}
 
 		public static void LogFormat(Object context, string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogFormat(context, format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogFormat(context, format, args);
+			}
 		}
 
 		public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogFormat(logType, logOptions, context, format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogFormat(logType, logOptions, context, format, args);
+			}
 		}
 
 		public static void LogFormat(string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogFormat(format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogFormat(format, args);
+			}
 		}
 
 		public static void LogWarning(object message, Object context)
 		{
-#if UNITY_EDITOR
-			Debug.LogWarning(message, context);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogWarning(message, context);
+			}
 		}
 
 		public static void LogWarning(object message)
 		{
-#if UNITY_EDITOR
-			Debug.LogWarning(message);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogWarning(message);
+			}
 		}
 
 		public static void LogWarningFormat(Object context, string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogWarningFormat(context, format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogWarningFormat(context, format, args);
+			}
 		}
 
 		public static void LogWarningFormat(string format, params object[] args)
 		{
-#if UNITY_EDITOR
-			Debug.LogWarningFormat(format, args);
-#endif
+			if (Debug.isDebugBuild)
+			{
+				Debug.LogWarningFormat(format, args);
+			}
 		}
 	}
 }
